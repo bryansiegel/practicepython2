70e04f16b148286030c15de444db7ed4952170d7
Bryan Siegel <bryansiegel@gmail.com>
2020-04-07 19:43:18 -0700

    number game

--- number-game.py
+++ number-game.py
@@ -1,50 +1,50 @@
-# Practice Python http://www.practicepython.org/exercise/2014/01/29/01-character-input.html
+# Number Game
 import random
 
+chances = 0
 
+# Max 3 chances
+while chances < 3:
+    # TODO: check input to make sure it's an int
+    guess = input('Enter A Random Number ')
+    answer = random.randrange(1,10)
+    # answer = 3
 
-# b = int(input(('Enter A Random Number ')))
-# f = random.randrange(0,3)
+# convert input guess variable into int
+    if int(guess) == answer:
+        print('Correct')
+        break
+    elif int(guess) != answer:
+        print('Wrong')
+        # print(type(guess))
+        # print(type(answer))
+        chances += 1
+# break out of the while loop once you hit 3 chances
+if chances == 3:
+    print('Sorry You have guessed wrong please try again')
+"""
+Old Code
+"""
+# try:
+#     b = input('Enter A Random Number ')
+#     f = random.randrange(1, 3)
 
+#     if int(b) != f:
+#         print('Wrong')
+#         b = input('Enter A Random Number Again')
+#     elif int(b) == f:
+#         print('Correct')
+# except ValueError:
+#     print('You Must Enter A Number. Please Try Again')
+#     b = input('Lets Try Again')
+#     if int(b) != f:
+#         print('Wrong Again')
+#     elif int(b) == f:
+#         print('Correct')
+"""
+Old Code End
+"""
 
-# print(type(b))
-
-def is_int():
-    try:
-        b = int(input('Enter A Random Number '))
-        f = random.randrange(0, 3)
-        if b == f:
-            print(f'Correct', f)
-        elif b != f:
-            print(f'Please Try Again',f)
-            b = int(input('Enter A Random Number Again '))
-        elif b == f:
-            print('You Are Right')
-        elif b != f:
-            print('Your Done')
-    except ValueError:
-        print('Not An Int Please Try Again')
-        b = int(input())
-
-is_int()
-
-# if (b):
-#     print('Not int')
-#     b = input()
-# elif int(b) == f:
-#     print(f'Correct', f)
-# elif int(b) != f:
-#     print('Incorrect')
-#     b = input()
-
-# if(type(b) != type(int)):
-#     print('Not Int')
-#     b = input()
-# if int(b) == f:
-#     print(f'Correct', f)
-# elif int(b) != f:
-#     print(f'2nd try Incorrect', f)
-#     b = input()
 
 
 
